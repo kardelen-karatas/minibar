6efab945fdf0b85ac36a5db1958c0a4e30cf96ab
kardelen-karatas <kardelen.karatas@gmail.com>
2020-09-26 16:18:07 +0200

    init auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,4 +10,23 @@ router.get('/signup', (req, res, next) => {
   res.render('auth/signup', {});
 });
 
+router.post('/signup', (req, res, next) => {
+  res.redirect('auth/signup');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login');
+})
+
+router.get('/login', (req, res, next) => {
+  res.redirect('auth/login');
+})
+
+router.post('/logout', (req, res, next) => {
+  res.redirect('index');
+})
+
+router.get('/userProfile', (req, res, next) => {
+  res.render('users/user-profile');
+})
 module.exports = router;
