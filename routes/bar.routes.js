2fb7d3e4edc717bea5a9609942c717a6919b7a14
Clementine <clementinenocton@hotmail.fr>
2020-10-06 21:38:54 +0200

    In progress

--- routes/bar.routes.js
+++ routes/bar.routes.js
@@ -5,29 +5,30 @@ const router = express.Router();
 module.exports = router;
 
 const Bar = require('../models/Bar.model.js');
+const fileUploader = require('../configs/cloudinary.config');
 
 
 router.get('/bars/new', (req, res, next) => {
 
   if(!req.session.currentUser){
-    res.redirect('/login')
+    res.redirect('/login');
   }
   res.render('bars/new', { userInSession: req.session.currentUser });
 });
 
-router.post('/bars/new', (req, res, next) => {
+router.post('/bars/new', fileUploader.single('image'), (req, res, next) => {
   // creation d'un bar
   const { name, address, minimumCb } = req.body;
-  const user_id = req.session.currentUser._id
+  const user_id = req.session.currentUser._id;
 
-  if(name === '' || address === '' || minimumCb === ''){
+  if (name === '' || address === '' || minimumCb === '') {
     res.render('bars/new', {userInSession: req.session.currentUser, errorMessage: 'All fields are mandatory. Please provide bar name, address and min payment by cart.' });
     return;
   }
 
-  Bar.create({ name, address, minimumCb, user_id })
+  Bar.create({ name, address, minimumCb, user_id, imageUrl: req.file.path })
     .then((newBar) => {
-      console.log(user_id)
+      console.log(req.file.path);
       res.redirect('/bars');
     })
     .catch((err) => {
@@ -35,8 +36,8 @@ router.post('/bars/new', (req, res, next) => {
         res.status(500).render('bars/new', { errorMessage: err.message });
       }
       else {
-        next(err)
-      };
+        next(err);
+      }
   });
 });
 
@@ -57,15 +58,15 @@ router.get('/bars', (req, res, next) => {
     // check if the logged in user is creator of the bar
     function isCreator(bar){
       if(req.session.currentUser && bar.user_id.toString() === req.session.currentUser._id){
-        return true
+        return true;
       }
-      return false
+      return false;
     }
 
     Bar.findById(id)
       .then((theBar) => {
       
-        res.render("bars/show", { bar: theBar, userInSession: req.session.currentUser, isCreator: isCreator(theBar)})
+        res.render("bars/show", { bar: theBar, userInSession: req.session.currentUser, isCreator: isCreator(theBar)});
       
       })
       .catch((error) => {
@@ -79,14 +80,14 @@ router.get('/bars/:id/edit', (req, res, next) => {
   const { id } = req.params;
 
   if(!req.session.currentUser){
-    res.redirect('/login')
+    res.redirect('/login');
   }
 
   Bar.findById(id)
   .then(barToEdit => {
 
     if(barToEdit.user_id.toString() !== req.session.currentUser._id){
-      res.redirect(`/bars/${barToEdit.id}`)
+      res.redirect(`/bars/${barToEdit.id}`);
     }
 
     res.render('bars/edit', {bar: barToEdit, userInSession: req.session.currentUser});
@@ -100,7 +101,7 @@ router.post('/bars/:id/edit', (req, res, next) => {
   const { id } = req.params;
   const { name, address, minimumCb } = req.body;
 
-  console.log(typeof(minimumCb))
+  console.log(typeof(minimumCb));
    
   Bar.findByIdAndUpdate(id, { name, address, minimumCb }, { new: true })
     .then(updatedBar => res.redirect(`/bars/${updatedBar.id}`))
